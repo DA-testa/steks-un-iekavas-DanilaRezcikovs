ece454790b3580b563ff1c6ac2cfb390918237e1
DanilaRezcikovs <125744863+DanilaRezcikovs@users.noreply.github.com>
2023-02-18 14:23:07 +0000

    commit

--- main.py
+++ main.py
@@ -11,20 +11,30 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next_char, i))
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+        if next_char in ")]}":
+            # Process closing bracket
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next_char):
+                return i + 1
+
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack[0].position + 1
+
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
